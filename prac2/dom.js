e598e708b9634e14ac9c0d1e86949b4420efda8b
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-06-17 16:18:00 +0900

    Prac2: add operation function skeleton

--- prac2/dom.js
+++ prac2/dom.js
@@ -12,3 +12,47 @@ window.addEventListener('load',function(){
         bod.value += e.target.value;
     }
 })
+
+//add function
+window.addEventListener('load',function(){
+    var calculator = document.querySelector(".calculator");
+    var btn = calculator.querySelector("add");
+    var bod = calculator.querySelector(".board");
+    
+    btn.onclick = function(e){
+        
+    }
+})
+
+//sub function
+window.addEventListener('load',function(){
+    var calculator = document.querySelector(".calculator");
+    var btn = calculator.querySelector("sub");
+    var bod = calculator.querySelector(".board");
+    
+    btn.onclick = function(e){
+        
+    }
+})
+
+//mul function
+window.addEventListener('load',function(){
+    var calculator = document.querySelector(".calculator");
+    var btn = calculator.querySelector("mul");
+    var bod = calculator.querySelector(".board");
+    
+    btn.onclick = function(e){
+        
+    }
+})
+
+//div function
+window.addEventListener('load',function(){
+    var calculator = document.querySelector(".calculator");
+    var btn = calculator.querySelector("div");
+    var bod = calculator.querySelector(".board");
+    
+    btn.onclick = function(e){
+
+    }
+})
