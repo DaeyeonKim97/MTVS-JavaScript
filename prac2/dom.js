8ccd36ee1b9aadd7d10ce32da4c663529acf01c5
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-06-18 23:17:55 +0900

    Prac2: add assignment operator function

--- prac2/dom.js
+++ prac2/dom.js
@@ -7,52 +7,14 @@ window.addEventListener('load',function(){
     btns.onclick = function(e){
         if(e.target.nodeName != "INPUT")
             return;
+        
+        if(e.target.value == "="){
+            // console.log(bod.value);
+            bod.value = eval(bod.value);
+            return;
+        }
 
-        console.log(e.target.value);
+        // console.log(e.target.value);
         bod.value += e.target.value;
-    }
-})
-
-//add function
-window.addEventListener('load',function(){
-    var calculator = document.querySelector(".calculator");
-    var btn = calculator.querySelector("add");
-    var bod = calculator.querySelector(".board");
-    
-    btn.onclick = function(e){
-        
-    }
-})
-
-//sub function
-window.addEventListener('load',function(){
-    var calculator = document.querySelector(".calculator");
-    var btn = calculator.querySelector("sub");
-    var bod = calculator.querySelector(".board");
-    
-    btn.onclick = function(e){
-        
-    }
-})
-
-//mul function
-window.addEventListener('load',function(){
-    var calculator = document.querySelector(".calculator");
-    var btn = calculator.querySelector("mul");
-    var bod = calculator.querySelector(".board");
-    
-    btn.onclick = function(e){
-        
-    }
-})
-
-//div function
-window.addEventListener('load',function(){
-    var calculator = document.querySelector(".calculator");
-    var btn = calculator.querySelector("div");
-    var bod = calculator.querySelector(".board");
-    
-    btn.onclick = function(e){
-
     }
 })
