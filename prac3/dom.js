344ad08257e082a4d9c4ea3b96614ba0890aedce
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-06-20 16:27:31 +0900

    Prac3: add btn-priv function

--- prac3/dom.js
+++ prac3/dom.js
@@ -1,3 +1,5 @@
+var offset = 1;
+
 //next-btn
 window.addEventListener('load',function(){
     // var mainSection = document.querySelector(".main-section");
@@ -6,7 +8,7 @@ window.addEventListener('load',function(){
     var btn = document.querySelector("#btn-next");
     console.log("btn");
 
-    var offset = 1;
+    // var offset = 1;
     var size = cards.length;
 
     btn.onclick = function(e){
@@ -15,6 +17,31 @@ window.addEventListener('load',function(){
         cards[(2+offset)%size].className = "third";
 
         offset++;
+    }
+    
+})
+
+//prev-btn
+window.addEventListener('load',function(){
+    // var mainSection = document.querySelector(".main-section");
+    var cards = document.querySelectorAll(".card-section>div");
+    // var btnSection = mainSection.querySelector(".btn-section");
+    var btn = document.querySelector("#btn-prev");
+    console.log("btn");
+
+    // var offset = 1;
+    var size = cards.length;
+
+    btn.onclick = function(e){
+        cards[(0+offset)%size].className = "third"; //card2
+        cards[(1+offset)%size].className = "first"; //card3
+        cards[(2+offset)%size].className = "second"; //card1
+
+        if(offset<1){
+            offset+=3;
+        }
+
+        offset--;
     }
     
 })
